07af290dfa1ed7b54b22397a1b109f5d826594d8
skyxt <xiangtian.yang@aliyun.com>
2023-10-08 20:47:14 +0800

    优化文件加载路径写法

--- ClassLoaderDemo/src/main/java/personal/skyxt/Main.java
+++ ClassLoaderDemo/src/main/java/personal/skyxt/Main.java
@@ -2,6 +2,8 @@ package personal.skyxt;
 
 import test.ClassLoaderPrint;
 
+import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.MalformedURLException;
@@ -15,17 +17,22 @@ public class Main {
         URL v2Url = null;
         try {
             //添加判断，判断jar包是否存在，不存在要抛出异常警告
-            v1Url = new URL("file:C:\\Users\\skyxt\\IdeaProjects\\JavaSample\\ClassLoaderDemo\\lib\\v1\\ClassLoaderPrint.jar");
-            v2Url = new URL("file:C:\\Users\\skyxt\\IdeaProjects\\JavaSample\\ClassLoaderDemo\\lib\\v2\\ClassLoaderPrint.jar");
-            URLClassLoader v1ClassLoader = new URLClassLoader(new URL[]{v1Url}, null);
+            String basePath = new File("./ClassLoaderDemo/lib").getCanonicalFile().getPath();
+            v1Url = new File(basePath + File.separator + "v1/ClassLoaderPrint.jar").toURI().toURL();
+            v2Url = new File(basePath + File.separator + "v2/ClassLoaderPrint.jar").toURI().toURL();
+            URLClassLoader v1ClassLoader = new URLClassLoader(new URL[]{ v1Url }, null);
             Class<?> v1Class = Class.forName("test.ClassLoaderPrint", true, v1ClassLoader);
             Method printv1 = v1Class.getMethod("print", null);
+            System.out.println("---------------");
             printv1.invoke(v1Class.newInstance(), null);
+            System.out.println("---------------");
 
-            URLClassLoader v2ClassLoader = new URLClassLoader(new URL[]{v2Url}, null);
+            URLClassLoader v2ClassLoader = new URLClassLoader(new URL[]{ v2Url }, null);
             Class<?> v2Class = Class.forName("test.ClassLoaderPrint", true, v2ClassLoader);
             Method printv2 = v2Class.getMethod("print", null);
+            System.out.println("---------------");
             printv2.invoke(v1Class.newInstance(), null);
+            System.out.println("---------------");
             /*
             //以下方式会导致ClassCastException
             Class<?> aClass = Class.forName("test.ClassLoaderPrint", true, urlClassLoader);
@@ -42,6 +49,8 @@ public class Main {
             e.printStackTrace();
         } catch (NoSuchMethodException e) {
             e.printStackTrace();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
 
 
