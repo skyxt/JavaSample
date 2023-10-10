08144303c9c0ea127eb6296c56d4fd5158df4987
skyxt <xiangtian.yang@aliyun.com>
2023-10-10 10:21:36 +0800

    新增破坏双亲委派模式demo1

--- ClassLoaderDemo/src/main/java/personal/skyxt/Main.java
+++ ClassLoaderDemo/src/main/java/personal/skyxt/Main.java
@@ -1,7 +1,5 @@
 package personal.skyxt;
 
-import test.ClassLoaderPrint;
-
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
@@ -11,12 +9,17 @@ import java.net.URL;
 import java.net.URLClassLoader;
 
 public class Main {
+    /**
+     * TODO
+     * @param args
+     */
 
     public static void main(String[] args) {
         URL v1Url = null;
         URL v2Url = null;
         try {
             //TODO 添加判断，判断jar包是否存在，不存在要抛出异常警告
+            //
             String basePath = new File("./ClassLoaderDemo/lib").getCanonicalFile().getPath();
             v1Url = new File(basePath + File.separator + "v1/ClassLoaderPrint.jar").toURI().toURL();
             v2Url = new File(basePath + File.separator + "v2/ClassLoaderPrint.jar").toURI().toURL();
@@ -24,11 +27,25 @@ public class Main {
             print(v2Url);
 
             /*
-            //以下方式会导致ClassCastException
+            //以下方式可能会导致ClassCastException
             Class<?> aClass = Class.forName("test.ClassLoaderPrint", true, urlClassLoader);
             ClassLoaderPrint v1Print = (ClassLoaderPrint) aClass.newInstance();
             v1Print.print();
             */
+
+            //设置当前上下文类加载器未自定义的类加载器，破坏双亲委派模式，测试的原生双亲委派模式下的java类还能否获取（结论：无法获取）
+            System.out.println("当前线程原始ClassLoader: " + Thread.currentThread().getContextClassLoader().getClass().getName());
+//            URLClassLoader urlClassLoader = new URLClassLoader(new URL[] {new File(basePath + File.separator + "v1/ClassLoaderPrint.jar").toURI().toURL()},null);
+            WebAppClassLoader urlClassLoader = new WebAppClassLoader(new URL[] {new File(basePath + File.separator + "v1/ClassLoaderPrint.jar").toURI().toURL()});
+            Thread.currentThread().setContextClassLoader(urlClassLoader);
+            System.out.println("修改当前线程Classloader为：" + urlClassLoader.getClass().getName());
+//            Class<?> aClass = urlClassLoader.loadClass("java.lang.Object");
+            Class<?> aClass = Class.forName("java.lang.Object", true, urlClassLoader);
+            Method toString = aClass.getMethod("toString", null);
+            Object invoke = toString.invoke(aClass.newInstance(), null);
+            System.out.println(invoke);
+            Thread.currentThread().setContextClassLoader(null);
+
         } catch (MalformedURLException | ClassNotFoundException e) {
             e.printStackTrace();
         } catch (InstantiationException e) {
--- ClassLoaderDemo/src/main/java/personal/skyxt/WebAppClassLoader.java
+++ ClassLoaderDemo/src/main/java/personal/skyxt/WebAppClassLoader.java
@@ -1,6 +1,33 @@
 package personal.skyxt;
 
-public class WebAppClassLoader extends ClassLoader{
+import java.net.URL;
+import java.net.URLClassLoader;
+
+public class WebAppClassLoader extends URLClassLoader {
+
+    public WebAppClassLoader(URL[] urls) {
+        super(urls, null);
+    }
+
+    //TODO 先查本ClassLoader,如果没有再找父加载器
+    @Override
+    public Class<?> loadClass(String name) throws ClassNotFoundException {
+        synchronized (getClassLoadingLock(name)) {
+            // First, check if the class has already been loaded
+            Class<?> c = findLoadedClass(name);
+            if (c == null) {
+                long t0 = System.nanoTime();
+
+                if (c == null) {
+                    // If still not found, then invoke findClass in order
+                    // to find the class.
+                    long t1 = System.nanoTime();
+                    c = findClass(name);
+                }
+            }
+            return c;
+        }
+    }
 
     @Override
     protected Class<?> findClass(String name) throws ClassNotFoundException {
